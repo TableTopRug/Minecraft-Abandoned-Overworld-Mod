f8939de0446d1c26e6132986f810088041e1dd78
TableTopRug <flame02fire@gmail.com>
2024-07-23 19:02:47 -0500

    add obscura ore and tab

--- src/main/java/tabletoprug/abandonedoverworld/AbandonedOverworld.java
+++ src/main/java/tabletoprug/abandonedoverworld/AbandonedOverworld.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+import tabletoprug.abandonedoverworld.client.AbandonedOverworldTab;
 import tabletoprug.abandonedoverworld.proxy.CommonProxy;
 
 
@@ -17,6 +18,7 @@ public class AbandonedOverworld {
     public static final String NAME = "Abandoned Overworld";
     public static final String VERSION = "0.0.1";
     public static final String MC_VERSION = "[1.12.2]";
+    public static final AbandonedOverworldTab TAB = new AbandonedOverworldTab();
 
     public static Logger logger;
 
--- src/main/java/tabletoprug/abandonedoverworld/block/BlockOre.java
+++ src/main/java/tabletoprug/abandonedoverworld/block/BlockOre.java
@@ -2,15 +2,23 @@ package tabletoprug.abandonedoverworld.block;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.oredict.OreDictionary;
 
 
 public class BlockOre extends BlockBase {
+    private String oreName;
 
-    public BlockOre(String name) {
+    public BlockOre(String name, String oreName) {
         super(Material.ROCK, name);
+
+        this.oreName = oreName;
 
         setHardness(3f);
         setResistance(5f);
+    }
+
+    public void initOreDict() {
+        OreDictionary.registerOre(oreName, this);
     }
 
     @Override
