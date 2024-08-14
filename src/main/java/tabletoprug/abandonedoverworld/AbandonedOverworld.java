d8db080eb696dd3f6a97058c1492d8991816f62d
TableTopRug <flame02fire@gmail.com>
2024-08-14 16:12:13 -0500

    gradle can sign

--- src/main/java/tabletoprug/abandonedoverworld/AbandonedOverworld.java
+++ src/main/java/tabletoprug/abandonedoverworld/AbandonedOverworld.java
@@ -15,12 +15,12 @@ import tabletoprug.abandonedoverworld.recipes.ModRecipes;
 import tabletoprug.abandonedoverworld.world.ModWorldGen;
 
 
-@Mod(modid = AbandonedOverworld.MODID, name = AbandonedOverworld.NAME, version = AbandonedOverworld.VERSION, acceptedMinecraftVersions = AbandonedOverworld.MC_VERSION)
+@Mod(useMetadata = true, modid = AbandonedOverworld.MODID, certificateFingerprint = "7ffb849242ce75b5313e506c31503f92ae75e999a4513148b6f4003108a42dec")
 public class AbandonedOverworld {
     public static final String MODID = "abandonedoverworld";
     public static final String NAME = "Abandoned Overworld";
-    public static final String VERSION = "0.9.0";
-    public static final String MC_VERSION = "[1.12.2]";
+//    public static final String VERSION = "0.9.0";
+//    public static final String MC_VERSION = "[1.12.2]";
     public static final AbandonedOverworldTab TAB = new AbandonedOverworldTab();
 
     public static Logger logger;
